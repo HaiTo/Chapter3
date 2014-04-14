e2f9f7d455da9e4b2715b07fb1f341e76f4d7e0c
haito <11jk077@ms.dendai.ac.jp>
2014-04-14 16:55:03 +0900

    User一覧画面（Index）の制作完了

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
 	factory :user do
-		name	"HaiTo"
-		email	"holloword@me.com"
-		password	"Rilinwing324"
-		password_confirmation	"Rilinwing324"
+		sequence(:name) { |n| "Person #{n}" }
+		sequence(:email){ |n| "person_#{n}@example.com"}
+		password	"foobar"
+		password_confirmation	"foobar"
 	end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,18 +5,23 @@ describe "User Pages" do
 
   # indexPageに関するTest
   describe "index" do
-    before do #User自動生成
-      sign_in FactoryGirl.create(:user)
-      FactoryGirl.create(:user,name:"Bob",email:"bob@example.com")
-      FactoryGirl.create(:user,name:"Alice",email:"Alice@example.com")
+    let(:user) { FactoryGirl.create(:user) }
+    before(:each) do
+      sign_in user
       visit users_path
     end
+
     it { should have_title('All users') }
     it { should have_content('All users') }
 
-    it "should list each user" do
-      User.all.each do |user|
-        expect(page).to have_selector('li',text: user.name)
+    describe "pagination" do
+      before(:all){30.times {FactoryGirl.create(:user) } }
+      after(:all){ User.delete_all }
+      it { should have_selector('div.pagination') }
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          expect(page).to have_selector('li',text: user.name)
+        end
       end
     end
   end
