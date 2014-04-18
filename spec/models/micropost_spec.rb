1d8fa719920f2ed2144c0e6b8a6dc7c31745ec91
haito <11jk077@ms.dendai.ac.jp>
2014-04-18 13:17:07 +0900

    MicroPostModelのInReplyToのテスト生成

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -17,31 +17,41 @@ describe "Microposts" do
   # 検証
   it { should be_valid }
 
-  # マイクロソフトがユーザーに紐付けられていること
+  # MicroPostがユーザーに紐付けられていること
   describe "when user_id is not persent" do
     before { @microposts.user_id = nil }
     it { should_not be_valid }
   end
   
-  # マイクロソフトがブランクである場合
+  # MicroPostがブランクである場合
   describe "with blank content" do
     before { @microposts.content = "" }
     it { should_not be_valid }
   end
 
-  # マイクロソフトが長すぎる場合
+  # Micropostが長すぎる場合
   describe "with content that is too long" do
     before { @microposts.content = "a"*141}
     it { should_not be_valid }
   end
 
-  # まいくろぽすとのInReplyToについてのテスト
+  # In_Reply_Toに対するテスト郡
   describe "with have in_reply_to" do
     let(:user) { FactoryGirl.create(:user) }
-    #let(:reply_user) { FactoryGirl.create(:user,name:"ReplyMan") }
+    let(:reply_user) { FactoryGirl.create(:user,name:"ReplyMan") }
     let(:micropost) { FactoryGirl.create(:micropost,user:user,content:"@ReplyMan hoge") }
     before do
     end
-    it {p micropost}
+    #it {p micropost}
+    #it {p reply_user }
+    describe "should not have in_reply_to" do
+      before { micropost.in_reply_to = reply_user.name}
+      it "should not have in_reply_to" do
+        expect(micropost.in_reply_to).not_to eq "Example User"
+      end
+      it "should have in_reply_to to ReplyMan" do
+        expect(micropost.in_reply_to).to eq reply_user.name
+      end
+    end
   end
 end
